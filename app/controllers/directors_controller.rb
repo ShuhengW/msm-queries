659d88272af4d9b5af862198064969639e613f04
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-08 22:27:58 +0000

    fix

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -26,19 +26,19 @@ class DirectorsController < ApplicationController
 
   def show
     the_id = params.fetch("directors_id")
-    @the_director = Director.where({ :id => the_id }).at(0)
+    @the_director = Director.where({ :id => the_id }).first
     render({ :template => "director_templates/show" })
   end
 
   def show_actor
     the_id = params.fetch("actors_id")
-    @the_actor = Actor.where({ :id => the_id }).at(0)
+    @the_actor = Actor.where({ :id => the_id }).first
     render({ :template => "director_templates/show_actor" })
   end
 
   def show_movie
     the_id = params.fetch("movies_id")
-    @the_movie = Movie.where({ :id => movies_id }).at(0)
+    @the_movie = Movie.where({ :id => movies_id }).first
     render({ :template => "director_templates/show_movie" })
   end
 end
